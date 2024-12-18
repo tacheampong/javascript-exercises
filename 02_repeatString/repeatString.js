92f18c9ce5d6ee5550c15f0d29ada19a5ddce3c2
tacheampong <th.acheampong@gmail.com>
2024-12-18 17:10:00 -0500

    repeat and helloworld

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(word, freq) {
 
+    let newWord = '';
+    if(freq < 0){
+        newWord = 'ERROR';
+        return newWord;
+    } 
+    for(let i = 1; i <= freq; i++){
+     if(freq === 0 || word === ''){
+        return newWord;
+        }
+        newWord = newWord + word;
+    }
+    return newWord;
 };
 
 // Do not edit below this line
