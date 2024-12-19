bb827c2f6df7d06ec975005e587aee872022fc82
tacheampong <th.acheampong@gmail.com>
2024-12-19 10:02:50 -0500

    reverseString function implemented

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(words) {
 
+    let newWords = words.split("");
+    let reverseWord = '';
+    if(words === ''){
+        return reverseWord;
+    }
+    for(let i = newWords.length - 1; i >= 0; i--){
+        reverseWord = reverseWord + newWords[i];
+    }
+    return reverseWord
 };
 
 // Do not edit below this line
