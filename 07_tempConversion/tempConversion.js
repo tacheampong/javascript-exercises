aee0a2181834a19c24f9f985ec27d4c4c33a24ff
tacheampong <th.acheampong@gmail.com>
2024-12-19 17:00:09 -0500

    tempConversion implemented

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+// fahrenheit to celsius
+const convertToCelsius = function(num) {
+  let conversion = 0;
+  conversion = (num - 32) * (5/9)
+  return Math.round(conversion * 10) /10;
 };
 
-const convertToFahrenheit = function() {
+// celsius to fahrenheit
+const convertToFahrenheit = function(num) {
+  let conversion = 0;
+  conversion = (num * (9/5) + 32);
+  return Math.round(conversion * 10) / 10;
 };
 
 // Do not edit below this line
