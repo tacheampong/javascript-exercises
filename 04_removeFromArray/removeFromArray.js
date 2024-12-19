db9c7f77a1a9821b7c43f3337ca0ab3df44295de
tacheampong <th.acheampong@gmail.com>
2024-12-19 13:59:28 -0500

    removeFromArray is implemented

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...arg) {
+
+    let newArray = [];
+    let isArg = false;
+    for(let i = 0; i < array.length; i++){
+        isArg = false;
+        for(let j = 0; j < arg.length; j++){
+            if(array[i] === arg[j]){
+                isArg = true;
+                continue;
+            }
+
+        }
+        if(isArg){
+            continue;
+        }
+        newArray.push(array[i]);      
+    }
+    return newArray;
 };
 
 // Do not edit below this line
