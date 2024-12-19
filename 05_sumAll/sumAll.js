c6f8a67ba8ce3f8891c579a7979152fd81bf70d4
tacheampong <th.acheampong@gmail.com>
2024-12-19 16:23:54 -0500

    sumAll is implemented

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    let total = 0;
+    if(typeof(num1) != "number" || typeof(num2) != "number" ) {
+        return 'ERROR';
+    }
+    if( (num1 % 1) !== 0 || (num2 % 1) !== 0){
+        return 'ERROR';
+    }
+    
+    if(num1 < 0 || num2 < 0){
+        return 'ERROR';
+    }
+    if(num1 > num2){
+
+     return sumAll(num2,num1);
+     }
+
+    for(let  i = num1; i <= num2; i++ ){
+        total = total + i;
+        
+    }
+    return total;
+    
 };
 
 // Do not edit below this line
